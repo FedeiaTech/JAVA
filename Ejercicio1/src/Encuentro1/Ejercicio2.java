74d85e855f560cdf46676c34db386e4c2dfcff75
Fede <iaconofede@gmail.com>
2023-03-12 23:55:45 -0300

    Primer commit

---
+++ Ejercicio1/src/Encuentro1/Ejercicio2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encuentro1;
+
+/**
+ *
+ * @author iacon
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+	int num1 = 5;
+	int num2 = 5;
+    
+	int suma = num1 + num2;
+    
+	double division = num1 / num2;
+    
+	boolean logico = num2 < num1;
+    
+	num1++;
+
+        System.out.println(division);
+         System.out.println(logico);
+          System.out.println(suma);
+    }
+}
---
+++ Ejercicio1/src/Encuentro1/Imprime.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Encuentro1;
+
+/**
+ *
+ * @author iacon
+ */
+public class Imprime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String nombre = "Fede";
+    short edad = 33;
+        System.out.println(nombre + " " + edad);
+    }
+    
+}

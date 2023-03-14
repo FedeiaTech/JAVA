4d8d349352f9095b24fc39f489c362997deb9900
Fede <iaconofede@gmail.com>
2023-03-13 22:00:24 -0300

    ejercicios guia 3

---
+++ Guia 3 - Ejercicios Practicos/src/guia/pkg3/ejercicios/practicos/Ejercicio1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia.pkg3.ejercicios.practicos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author iacon
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ingrese = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero");
+        int num = ingrese.nextInt();
+
+        if (num%2 == 0) {
+            System.out.println("El numero es par");
+        } else {
+            System.out.println("El numero es impar");
+        }
+    }
+}
+
+
---
+++ Guia3/src/guia3/Ejercicio6.java
@@ -0,0 +1,34 @@
+/*
+ Implementar un programa que le pida dos números enteros al usuario y determine 
+si ambos o alguno de ellos es mayor a 25.
+ */
+package guia3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author iacon
+ */
+public class Ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner ingrese = new Scanner(System.in);
+    System.out.println("Ingrese dos numeros");
+    int num1 = ingrese.nextInt();
+    int num2 = ingrese.nextInt();
+    
+    if (  num1 > 25 || num2 > 25 ){ 
+        System.out.println("alguno o ambos son mayores a 25");
+    }
+    else{
+    System.out.println("ninguno es mayor a 25");
+    }
+        
+        
+    }
+    
+}

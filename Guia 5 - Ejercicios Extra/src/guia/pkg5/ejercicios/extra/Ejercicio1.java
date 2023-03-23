d0d5e60c3bb2480cb964c91e7922b8d4a6890162
Fede <iaconofede@gmail.com>
2023-03-23 20:19:11 -0300

    Ejercicios rev 23/3/23

--- Guia 5 - Ejercicios Extra/src/guia/pkg5/ejercicios/extra/Ejercicio1.java
+++ Guia 5 - Ejercicios Extra/src/guia/pkg5/ejercicios/extra/Ejercicio1.java
@@ -5,8 +5,11 @@
  */
 package guia.pkg5.ejercicios.extra;
 
+import java.util.Scanner;
+
 /**
- *
+ * Realizar un algoritmo que calcule la suma de todos los elementos de un 
+ * vector de tamaño N, con los valores ingresados por el usuario.
  * @author iacon
  */
 public class Ejercicio1 {
@@ -15,7 +18,27 @@ public class Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner ingrese = new Scanner(System.in);
+        
+        System.out.println("Cuantos numeros va a sumar?");
+        int n = ingrese.nextInt();
+        
+        int[] vector = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("Ingrese el numero a sumar");
+            vector[i] = ingrese.nextInt();
+        }
+        
+        int suma = 0;
+        System.out.println("Se sumaran los siguientes numeros:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(vector[i] + " | ");
+            suma += vector[i];
+        }
+        System.out.println("");
+            
+            System.out.println("R: " + suma);
     }
     
 }
